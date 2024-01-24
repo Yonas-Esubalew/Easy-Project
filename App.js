4eb7fcb39a04864b5d3230b8a0af1d9fef32f77d
Yonas-Esubalew <157085551+Yonas-Esubalew@users.noreply.github.com>
2024-01-24 10:03:07 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,31 +1,9 @@
-import React, { useState } from 'react'
-import Menu from './Menu'
-import Catagories from './Catagories'
-import items from './data'
-const App = () => {
-    const [menuItems, setMenuItems] = useState(items)
-    const [catagories, setCatagories] = useState([])
-
-    const filterItems = (catagory) => {
-
-        if(catagory === 'all') {
-            setMenuItems(items)
-            return
-        }
-        const newItems = items.filter(()=> items.catagory === catagory);
-        setMenuItems(newItems)
-    }
+import React from 'react'
+import './App.css';
+function App() {
   return (
-    <div>
-        <section className='menu section'>
-            <div className='title'>
-                <h2>Our Menu</h2>
-                <div className='underline'></div>
-            </div>
-            <Catagories filterItems={filterItems}/>
-            <Menu items={menuItems}/>
-        </section>
-    </div>
-  )
+    <div>hello</div>
+  );
 }
-export default App
+
+export default App;
