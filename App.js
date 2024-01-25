7de7e39e1a892530d869c8f3d8666d1a8accc155
Yonas-Esubalew <157085551+Yonas-Esubalew@users.noreply.github.com>
2024-01-25 11:26:14 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,95 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import List from './List';
-import Alert from './Alert';
-const getLocalStorage = () => {
-  let list = localStorage.getItem('list');
-  if (list) {
-    return (list = JSON.parse(localStorage.getItem('list')));
-  } else {
-    return [];
-  }
-};
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
+
 function App() {
-  const [name, setName] = useState('');
-  const [list, setList] = useState(getLocalStorage());
-  const [isEditing, setIsEditing] = useState(false);
-  const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values('#f15025').all(10))
+
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!name) {
-      showAlert(true, 'danger', 'please enter value');
-    } else if (name && isEditing) {
-      setList(
-        list.map((item) => {
-          if (item.id === editID) {
-            return { ...item, title: name };
-          }
-          return item;
-        })
-      );
-      setName('');
-      setEditID(null);
-      setIsEditing(false);
-      showAlert(true, 'success', 'value changed');
-    } else {
-      showAlert(true, 'success', 'item added to the list');
-      const newItem = { id: new Date().getTime().toString(), title: name };
-
-      setList([...list, newItem]);
-      setName('');
+    e.preventDefault()
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
     }
-  };
+  }
 
-  const showAlert = (show = false, type = '', msg = '') => {
-    setAlert({ show, type, msg });
-  };
-  const clearList = () => {
-    showAlert(true, 'danger', 'empty list');
-    setList([]);
-  };
-  const removeItem = (id) => {
-    showAlert(true, 'danger', 'item removed');
-    setList(list.filter((item) => item.id !== id));
-  };
-  const editItem = (id) => {
-    const specificItem = list.find((item) => item.id === id);
-    setIsEditing(true);
-    setEditID(id);
-    setName(specificItem.title);
-  };
-  useEffect(() => {
-    localStorage.setItem('list', JSON.stringify(list));
-  }, [list]);
   return (
-    <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-
-        <h3>grocery bud</h3>
-        <div className='form-control'>
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
           <input
             type='text'
-            className='grocery'
-            placeholder='e.g. eggs'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
           />
-          <button type='submit' className='submit-btn'>
-            {isEditing ? 'edit' : 'submit'}
+          <button className='btn' type='submit'>
+            submit
           </button>
-        </div>
-      </form>
-      {list.length > 0 && (
-        <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} editItem={editItem} />
-          <button className='clear-btn' onClick={clearList}>
-            clear items
-          </button>
-        </div>
-      )}
-    </section>
-  );
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
