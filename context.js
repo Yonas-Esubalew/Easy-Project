14fc007fb8dd5c45fed3da6fa4fded3c1726cafb
Yonas-Esubalew <157085551+Yonas-Esubalew@users.noreply.github.com>
2024-01-29 08:36:27 -0800

    Add files via upload

--- context.js
+++ context.js
@@ -1,67 +1,48 @@
-import React, { useContext, useEffect, useReducer } from 'react'
-
-import {
-  SET_LOADING,
-  SET_STORIES,
-  REMOVE_STORY,
-  HANDLE_PAGE,
-  HANDLE_SEARCH,
-} from './actions'
-import reducer from './reducer'
-
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
-
-const initialState = {
-  isLoading: true,
-  hits: [],
-  query: 'react',
-  page: 0,
-  nbPages: 0,
-}
-
-const AppContext = React.createContext()
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  const fetchStories = async (url) => {
-    dispatch({ type: SET_LOADING })
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      dispatch({
-        type: SET_STORIES,
-        payload: { hits: data.hits, nbPages: data.nbPages },
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const removeStory = (id) => {
-    dispatch({ type: REMOVE_STORY, payload: id })
-  }
-  const handleSearch = (query) => {
-    dispatch({ type: HANDLE_SEARCH, payload: query })
-  }
-  const handlePage = (value) => {
-    dispatch({ type: HANDLE_PAGE, payload: value })
-  }
-  useEffect(() => {
-    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
-  }, [state.query, state.page])
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
 
   return (
     <AppContext.Provider
-      value={{ ...state, removeStory, handleSearch, handlePage }}
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
