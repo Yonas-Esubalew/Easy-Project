d5d865b2940c085f06980c824c2aa7c2125bf239
Yonas-Esubalew <157085551+Yonas-Esubalew@users.noreply.github.com>
2024-01-31 11:52:11 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import {Welcome} from './Components/Welcome';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Welcome></Welcome>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
