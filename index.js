83f8be90d5d33a85e40fd0790c1f9a7578f08c97
Yonas-Esubalew <157085551+Yonas-Esubalew@users.noreply.github.com>
2024-02-01 11:27:02 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-export { default as Blog } from './blog/Blog';
-export { default as Features } from './features/Features';
-export { default as Footer } from './footer/Footer';
-export { default as Header } from './header/Header';
-export { default as Possibility } from './possibility/Possibility';
-export { default as WhatGPT3 } from './whatGPT3/WhatGPT3';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
