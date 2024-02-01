0f2dc2aab606fc7b5d07b927340f42b66748a0fc
Yonas-Esubalew <157085551+Yonas-Esubalew@users.noreply.github.com>
2024-02-01 11:32:44 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Modal from './Modal';
-import Sidebar from './Sidebar';
-import Home from './Home';
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <>
-      <Home />
-      <Modal />
-      <Sidebar />
-    </>
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
--- data.js
+++ data.js
@@ -1,73 +1,33 @@
-import React from 'react';
-import {
-  FaBehance,
-  FaFacebook,
-  FaLinkedin,
-  FaTwitter,
-  FaSketch,
-  FaHome,
-  FaUserFriends,
-  FaFolderOpen,
-  FaCalendarAlt,
-  FaWpforms,
-} from 'react-icons/fa';
-export const links = [
+const questions = [
   {
     id: 1,
-    url: '/',
-    text: 'home',
-    icon: <FaHome />,
+    title: 'Do I have to allow the use of cookies?',
+    info:
+      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
   },
   {
     id: 2,
-    url: '/team',
-    text: 'team',
-    icon: <FaUserFriends />,
+    title: 'How do I change my My Page password?',
+    info:
+      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
   },
   {
     id: 3,
-    url: '/projects',
-    text: 'projects',
-    icon: <FaFolderOpen />,
+    title: 'What is BankID?',
+    info:
+      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
   },
   {
     id: 4,
-    url: '/calendar',
-    text: 'calendar',
-    icon: <FaCalendarAlt />,
+    title: 'Whose birth number can I use?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
   },
   {
     id: 5,
-    url: '/documents',
-    text: 'documents',
-    icon: <FaWpforms />,
+    title: 'When do I recieve a password ordered by letter?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
   },
-];
-
-export const social = [
-  {
-    id: 1,
-    url: 'https://www.twitter.com',
-    icon: <FaFacebook />,
-  },
-  {
-    id: 2,
-    url: 'https://www.twitter.com',
-    icon: <FaTwitter />,
-  },
-  {
-    id: 3,
-    url: 'https://www.twitter.com',
-    icon: <FaLinkedin />,
-  },
-  {
-    id: 4,
-    url: 'https://www.twitter.com',
-    icon: <FaBehance />,
-  },
-  {
-    id: 5,
-    url: 'https://www.twitter.com',
-    icon: <FaSketch />,
-  },
-];
+]
+export default questions
